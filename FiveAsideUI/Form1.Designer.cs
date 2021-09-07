8f3e6e300ee62371003a9b0957f7a258238347bb
Thomas DORRINGTON <016518@brgs.org.uk>
2021-09-07 17:36:28 +0100

    created main menu UI

---
+++ FiveAsideUI/Form1.Designer.cs
@@ -0,0 +1,114 @@
+﻿
+namespace FiveAsideUI
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.topTitleLabel = new System.Windows.Forms.Label();
+            this.bottomTitleLabel = new System.Windows.Forms.Label();
+            this.signUpButton = new System.Windows.Forms.Button();
+            this.loginButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // topTitleLabel
+            // 
+            this.topTitleLabel.AutoSize = true;
+            this.topTitleLabel.BackColor = System.Drawing.Color.Transparent;
+            this.topTitleLabel.Font = new System.Drawing.Font("Segoe UI Black", 30F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.topTitleLabel.ForeColor = System.Drawing.Color.DarkRed;
+            this.topTitleLabel.Location = new System.Drawing.Point(42, 34);
+            this.topTitleLabel.Name = "topTitleLabel";
+            this.topTitleLabel.Size = new System.Drawing.Size(1406, 106);
+            this.topTitleLabel.TabIndex = 0;
+            this.topTitleLabel.Text = "Five-A-Side Fantasy Dream League";
+            // 
+            // bottomTitleLabel
+            // 
+            this.bottomTitleLabel.AutoSize = true;
+            this.bottomTitleLabel.BackColor = System.Drawing.Color.Transparent;
+            this.bottomTitleLabel.Font = new System.Drawing.Font("Segoe UI Black", 40F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.bottomTitleLabel.ForeColor = System.Drawing.Color.Gold;
+            this.bottomTitleLabel.Location = new System.Drawing.Point(374, 140);
+            this.bottomTitleLabel.Name = "bottomTitleLabel";
+            this.bottomTitleLabel.Size = new System.Drawing.Size(704, 142);
+            this.bottomTitleLabel.TabIndex = 1;
+            this.bottomTitleLabel.Text = "SIMULATOR";
+            // 
+            // signUpButton
+            // 
+            this.signUpButton.BackColor = System.Drawing.Color.White;
+            this.signUpButton.Font = new System.Drawing.Font("Segoe UI Black", 13F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.signUpButton.Location = new System.Drawing.Point(235, 372);
+            this.signUpButton.Name = "signUpButton";
+            this.signUpButton.Size = new System.Drawing.Size(456, 179);
+            this.signUpButton.TabIndex = 2;
+            this.signUpButton.Text = "Sign up now! Create your dream team...";
+            this.signUpButton.UseVisualStyleBackColor = false;
+            this.signUpButton.Click += new System.EventHandler(this.signUpButton_Click);
+            // 
+            // loginButton
+            // 
+            this.loginButton.BackColor = System.Drawing.Color.White;
+            this.loginButton.Font = new System.Drawing.Font("Segoe UI Black", 13F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.loginButton.Location = new System.Drawing.Point(801, 372);
+            this.loginButton.Name = "loginButton";
+            this.loginButton.Size = new System.Drawing.Size(456, 179);
+            this.loginButton.TabIndex = 3;
+            this.loginButton.Text = "Already played? Log in to your account here!";
+            this.loginButton.UseVisualStyleBackColor = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(15F, 32F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(1470, 847);
+            this.Controls.Add(this.loginButton);
+            this.Controls.Add(this.signUpButton);
+            this.Controls.Add(this.bottomTitleLabel);
+            this.Controls.Add(this.topTitleLabel);
+            this.Font = new System.Drawing.Font("Segoe UI Black", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.ForeColor = System.Drawing.Color.Black;
+            this.Name = "Form1";
+            this.Text = "Five-A-Side Dream League Simulator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label topTitleLabel;
+        private System.Windows.Forms.Label bottomTitleLabel;
+        private System.Windows.Forms.Button signUpButton;
+        private System.Windows.Forms.Button loginButton;
+    }
+}
+
---
+++ FiveAsideUI/Form1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FiveAsideUI
+{
+    public partial class Form1 : Form
+    {
+        private void signUpButton_Click(object sender, EventArgs e)
+        {
+            
+            this.Hide();
+        }
+    }
+}
