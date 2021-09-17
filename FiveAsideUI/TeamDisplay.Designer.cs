99d1d85a5f4cc854da5c6d981f2cbb3a73d2759e
Thomas DORRINGTON <016518@brgs.org.uk>
2021-09-17 10:49:57 +0100

    'fifa card' for captain created in team display form

--- FiveAsideUI/TeamDisplay.Designer.cs
+++ FiveAsideUI/TeamDisplay.Designer.cs
@@ -31,6 +31,26 @@ namespace FiveAsideUI
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(TeamDisplay));
             this.topTitleLabel = new System.Windows.Forms.Label();
+            this.captainGroupBox = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.captainShirtNum = new System.Windows.Forms.Label();
+            this.captainSHO = new System.Windows.Forms.Label();
+            this.captainDRI = new System.Windows.Forms.Label();
+            this.captainPAC = new System.Windows.Forms.Label();
+            this.captainPHY = new System.Windows.Forms.Label();
+            this.captainREL = new System.Windows.Forms.Label();
+            this.captainTAC = new System.Windows.Forms.Label();
+            this.captainAGG = new System.Windows.Forms.Label();
+            this.captainName = new System.Windows.Forms.Label();
+            this.captainGroupBox.SuspendLayout();
             this.SuspendLayout();
             // 
             // topTitleLabel
@@ -45,6 +65,205 @@ namespace FiveAsideUI
             this.topTitleLabel.TabIndex = 2;
             this.topTitleLabel.Text = "Five-A-Side Dream League";
             // 
+            // captainGroupBox
+            // 
+            this.captainGroupBox.BackColor = System.Drawing.Color.Gold;
+            this.captainGroupBox.Controls.Add(this.captainName);
+            this.captainGroupBox.Controls.Add(this.captainAGG);
+            this.captainGroupBox.Controls.Add(this.captainTAC);
+            this.captainGroupBox.Controls.Add(this.captainREL);
+            this.captainGroupBox.Controls.Add(this.captainPHY);
+            this.captainGroupBox.Controls.Add(this.captainPAC);
+            this.captainGroupBox.Controls.Add(this.captainDRI);
+            this.captainGroupBox.Controls.Add(this.captainSHO);
+            this.captainGroupBox.Controls.Add(this.captainShirtNum);
+            this.captainGroupBox.Controls.Add(this.label9);
+            this.captainGroupBox.Controls.Add(this.label8);
+            this.captainGroupBox.Controls.Add(this.label7);
+            this.captainGroupBox.Controls.Add(this.label6);
+            this.captainGroupBox.Controls.Add(this.label5);
+            this.captainGroupBox.Controls.Add(this.label4);
+            this.captainGroupBox.Controls.Add(this.label3);
+            this.captainGroupBox.Controls.Add(this.label2);
+            this.captainGroupBox.Controls.Add(this.label1);
+            this.captainGroupBox.Location = new System.Drawing.Point(337, 181);
+            this.captainGroupBox.Name = "captainGroupBox";
+            this.captainGroupBox.Size = new System.Drawing.Size(363, 247);
+            this.captainGroupBox.TabIndex = 3;
+            this.captainGroupBox.TabStop = false;
+            this.captainGroupBox.Text = "Captain";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI Black", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(17, 135);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(73, 32);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "SHO:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI Black", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(26, 176);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(64, 32);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "DRI:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI Black", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label3.Location = new System.Drawing.Point(19, 212);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(71, 32);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "PAC:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Segoe UI Black", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label4.Location = new System.Drawing.Point(17, 49);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(90, 32);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Name:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Segoe UI Black", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label5.Location = new System.Drawing.Point(176, 93);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(73, 32);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "PHY:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Segoe UI Black", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label6.Location = new System.Drawing.Point(182, 135);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(64, 32);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "REL:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Segoe UI Black", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label7.Location = new System.Drawing.Point(176, 176);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(70, 32);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "TAC:";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Segoe UI Black", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label8.Location = new System.Drawing.Point(175, 212);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(74, 32);
+            this.label8.TabIndex = 7;
+            this.label8.Text = "AGG:";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Segoe UI Black", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label9.Location = new System.Drawing.Point(6, 93);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(99, 32);
+            this.label9.TabIndex = 8;
+            this.label9.Text = "Shirt #:";
+            // 
+            // captainShirtNum
+            // 
+            this.captainShirtNum.AutoSize = true;
+            this.captainShirtNum.Location = new System.Drawing.Point(112, 93);
+            this.captainShirtNum.Name = "captainShirtNum";
+            this.captainShirtNum.Size = new System.Drawing.Size(27, 32);
+            this.captainShirtNum.TabIndex = 9;
+            this.captainShirtNum.Text = "0";
+            // 
+            // captainSHO
+            // 
+            this.captainSHO.AutoSize = true;
+            this.captainSHO.Location = new System.Drawing.Point(112, 135);
+            this.captainSHO.Name = "captainSHO";
+            this.captainSHO.Size = new System.Drawing.Size(27, 32);
+            this.captainSHO.TabIndex = 10;
+            this.captainSHO.Text = "0";
+            // 
+            // captainDRI
+            // 
+            this.captainDRI.AutoSize = true;
+            this.captainDRI.Location = new System.Drawing.Point(112, 176);
+            this.captainDRI.Name = "captainDRI";
+            this.captainDRI.Size = new System.Drawing.Size(27, 32);
+            this.captainDRI.TabIndex = 11;
+            this.captainDRI.Text = "0";
+            // 
+            // captainPAC
+            // 
+            this.captainPAC.AutoSize = true;
+            this.captainPAC.Location = new System.Drawing.Point(112, 212);
+            this.captainPAC.Name = "captainPAC";
+            this.captainPAC.Size = new System.Drawing.Size(27, 32);
+            this.captainPAC.TabIndex = 12;
+            this.captainPAC.Text = "0";
+            // 
+            // captainPHY
+            // 
+            this.captainPHY.AutoSize = true;
+            this.captainPHY.Location = new System.Drawing.Point(288, 93);
+            this.captainPHY.Name = "captainPHY";
+            this.captainPHY.Size = new System.Drawing.Size(27, 32);
+            this.captainPHY.TabIndex = 13;
+            this.captainPHY.Text = "0";
+            // 
+            // captainREL
+            // 
+            this.captainREL.AutoSize = true;
+            this.captainREL.Location = new System.Drawing.Point(288, 135);
+            this.captainREL.Name = "captainREL";
+            this.captainREL.Size = new System.Drawing.Size(27, 32);
+            this.captainREL.TabIndex = 14;
+            this.captainREL.Text = "0";
+            // 
+            // captainTAC
+            // 
+            this.captainTAC.AutoSize = true;
+            this.captainTAC.Location = new System.Drawing.Point(288, 176);
+            this.captainTAC.Name = "captainTAC";
+            this.captainTAC.Size = new System.Drawing.Size(27, 32);
+            this.captainTAC.TabIndex = 15;
+            this.captainTAC.Text = "0";
+            // 
+            // captainAGG
+            // 
+            this.captainAGG.AutoSize = true;
+            this.captainAGG.Location = new System.Drawing.Point(288, 212);
+            this.captainAGG.Name = "captainAGG";
+            this.captainAGG.Size = new System.Drawing.Size(27, 32);
+            this.captainAGG.TabIndex = 16;
+            this.captainAGG.Text = "0";
+            // 
+            // captainName
+            // 
+            this.captainName.AutoSize = true;
+            this.captainName.Location = new System.Drawing.Point(112, 49);
+            this.captainName.Name = "captainName";
+            this.captainName.Size = new System.Drawing.Size(205, 32);
+            this.captainName.TabIndex = 17;
+            this.captainName.Text = "Placeholder name";
+            // 
             // TeamDisplay
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 32F);
@@ -52,10 +271,13 @@ namespace FiveAsideUI
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.ClientSize = new System.Drawing.Size(1894, 1009);
+            this.Controls.Add(this.captainGroupBox);
             this.Controls.Add(this.topTitleLabel);
             this.Name = "TeamDisplay";
             this.Text = "Team Display";
             this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.captainGroupBox.ResumeLayout(false);
+            this.captainGroupBox.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -64,5 +286,24 @@ namespace FiveAsideUI
         #endregion
 
         private System.Windows.Forms.Label topTitleLabel;
+        private System.Windows.Forms.GroupBox captainGroupBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label captainName;
+        private System.Windows.Forms.Label captainAGG;
+        private System.Windows.Forms.Label captainTAC;
+        private System.Windows.Forms.Label captainREL;
+        private System.Windows.Forms.Label captainPHY;
+        private System.Windows.Forms.Label captainPAC;
+        private System.Windows.Forms.Label captainDRI;
+        private System.Windows.Forms.Label captainSHO;
+        private System.Windows.Forms.Label captainShirtNum;
+        private System.Windows.Forms.Label label9;
     }
 }
